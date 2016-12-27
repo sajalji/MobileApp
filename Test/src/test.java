3a07de0067ac6310bd189e041521f482389046e0
sajal <sajal.singh@chargepoint.com>
2016-12-27 09:37:40 +0530

    Selenium started

--- Test/src/test.java
+++ Test/src/test.java
@@ -1,22 +1,29 @@
+import java.util.concurrent.TimeUnit;
 
-public class test {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class test{
+	
+{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sajal\\Desktop\\jars\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://mail.google.com");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+	
+	// xpath is chrome syntax $x('//*[@id="Email"]')
+	driver.findElement(By.xpath("//*[@id='Email']")).sendKeys("sam21088@gmail.com");
+	driver.findElement(By.xpath("/*[@id='next']")).click();
+	driver.findElement(By.xpath("//*[@id='password']")).sendKeys("sajalsighisthenextelonmusk");
+	driver.findElement(By.xpath("/*[@id='submit']")).click();
 
 	
-		
-	int fibo=-1;
 	
-	public int fibM(int n)
-	{
-		System.out.print(n);
-		if(n==1)
-		{
-			return 1;
-		}
-		else 
-			return fibM(n)+fibM(n-1);
-		
-	}
 	
-
 	
+}
 }
